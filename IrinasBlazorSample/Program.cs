b50484ea23b5c9fb461b50d48b6c1ecd68acbf4d
Michael Robertson <michael.robertson@archetypesc.com>
2020-03-31 09:37:43 -0400

    Add project files.

---
+++ IrinasBlazorSample/Program.cs
@@ -0,0 +1,40 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Sinks.SystemConsole.Themes;
+
+namespace IrinasBlazorSample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Log.Logger = new LoggerConfiguration()
+                    .WriteTo
+                    .Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}", theme: AnsiConsoleTheme.Literate)
+                    .CreateLogger();
+                var host = CreateHostBuilder(args).Build();
+
+                Log.Information("Starting Irina's Blazor App Host...");
+                throw new Exception("We blew up!");
+                host.Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "A Fatal Error occured in {source}", typeof(Program).FullName);
+                throw;
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseSerilog();
+                });
+    }
+}
